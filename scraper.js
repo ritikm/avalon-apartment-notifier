c180fa67f9749dce5df747ad54e8bb1bc461a64d
Ritik Malhotra <ritikmalhotra@gmail.com>
2015-09-24 19:26:45 -0500

    Fixed bugs, added undef and empty checks

--- scraper.js
+++ scraper.js
@@ -1,27 +1,42 @@
 #!/usr/bin/env node
 
+// Init node modules
 var request = require('request');
 var _ = require('underscore');
 var parseArgs = require('minimist');
 
+// Collect and parse the CLI arguments
 var args = parseArgs(process.argv.slice(2));
-console.dir(args);
-
-var minPrice = args.minPrice || 0;
-var maxPrice = args.maxPrice || 9999;
+var minRent = args.minRent || 0;
+var maxRent = args.maxRent || 9999;
 var numBedrooms = args.bedrooms || 2;
 var numBathrooms = args.bathrooms || 2;
 
-request('http://api.avalonbay.com/json/reply/ApartmentSearch?communityCode=CA067&min=' + minPrice + '&max=' + maxPrice + '&_=1443137667833', function (error, response, body) {
+// Make the request to the Avalon API
+request('http://api.avalonbay.com/json/reply/ApartmentSearch?communityCode=CA067&min=' + minRent + '&max=' + maxRent + '&_=1443137667833', function (error, response, body) {
   if (!error && response.statusCode == 200) {
+    // Parse the JSON output
     var aptData = JSON.parse(body);
+
+    // Filter down based on parameters
     var availableApts = aptData.results.availableFloorPlanTypes;
-    var twoBrApts = _.where(availableApts, { floorPlanTypeCode: (numBedrooms + "BD") })[0].availableFloorPlans;
-    var twoBrTwoBaApts = _.where(twoBrApts, { floorPlanBathType: (numBathrooms + " baths") });
-    var cheapTwoBrTwoBaApts = _.filter(twoBrTwoBaApts, function (e) {
-        var pricing = e.finishPackages[0].apartments[0].pricing;
-        return Math.min(pricing.amenitizedRent, pricing.effectiveRent) < 4900;
-    });
-    console.log(JSON.stringify(cheapTwoBrTwoBaApts));
+
+    if (!_.isUndefined(availableApts)) {
+        var bedroomFilter = _.where(availableApts, { floorPlanTypeCode: (numBedrooms + "BD") });
+
+        if (!_.isUndefined(bedroomFilter) && !_.isEmpty(bedroomFilter)) {
+            bedroomFilter = bedroomFilter[0].availableFloorPlans;
+
+            if (!_.isUndefined(bedroomFilter)) {
+                var bedBathFilter = _.where(bedroomFilter, { floorPlanBathType: (numBathrooms + " baths") });
+
+                if (!_.isEmpty(bedBathFilter)) {
+                    console.log(JSON.stringify(bedBathFilter));
+                } else {
+                    console.log("None available");
+                }
+            }
+        }
+    }
   }
 });
